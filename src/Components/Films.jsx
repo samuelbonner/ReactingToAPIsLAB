ebd40b5e616c07af3082bea4fe56e2326564bdab
Samuel Bonner <bonner.sam@gmail.com>
2021-07-01 16:45:27 -0500

    Tweaked Film card styling

--- src/Components/Films.jsx
+++ src/Components/Films.jsx
@@ -6,13 +6,16 @@ const Films = (films) => {
       <div className="col-sm-6 mb-3">
         <div className="card">
           <div className="card-body bg-light">
-            <h5 className="card-title">{films.films.title}</h5>
+            <h5 className="card-title text-center">{films.films.title}</h5>
             <p className="card-text">{films.films.description}</p>
-            <p className="card-text text-muted">
+            
+            <p className="card-text text-muted d-flex justify-content-evenly">
+            <span>
               Release Year: {films.films.release_date}
-            </p>
-            <p className="card-text text-muted">
+            </span>
+            <span>
               Run Time (min): {films.films.running_time}
+              </span>
             </p>
           </div>
         </div>
